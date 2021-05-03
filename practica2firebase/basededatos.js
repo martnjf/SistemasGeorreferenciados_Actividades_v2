ed40088b22703bb36cce4022210171e47d54face
martnjf <martin.jassof28@gmail.com>
2021-05-03 16:17:41 -0500

    practicas1y2firebasecorregidas

---
+++ practica2firebase/basededatos.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAtskMISz-kfWH-A-WnJvXpHBfQQNTL75U",
+    authDomain: "practica1firebase1.firebaseapp.com",
+    projectId: "practica1firebase1",
+    storageBucket: "practica1firebase1.appspot.com",
+    messagingSenderId: "1065846295266",
+    appId: "1:1065846295266:web:fd569d5e4baad1b1bb2cbb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
